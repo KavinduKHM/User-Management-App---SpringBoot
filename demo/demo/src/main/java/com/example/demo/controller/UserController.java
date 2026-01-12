dfebd167fa1f38e60e81b20e171cf6e2f3a7e188
Hewamaddumage K.K. <160339120+KaviiDream@users.noreply.github.com>
2026-01-13 00:02:35 +0530

    Add files via upload

---
+++ demo/demo/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+//Getting annotations for REST controller
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping(value="/api/v1")
+
+public class UserController {
+
+    @GetMapping("/getUser")
+    public String getUser() {
+        return "All Users";
+    }
+}
+
+
---
+++ demo/demo/src/main/java/com/example/demo/dto/UserDTO.java
@@ -0,0 +1,7 @@
+package com.example.demo.dto;
+
+public class UserDTO {
+
+    private int id;
+    private String name;
+}
