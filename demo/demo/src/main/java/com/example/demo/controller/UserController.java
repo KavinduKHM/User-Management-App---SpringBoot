c427086eacd303e410f8973c23ca765d619b4c94
Kavindu Kalhara <kavinduhewamadduma@gmail.com>
2026-01-20 01:31:27 +0530

    Update UserController.java

--- demo/demo/src/main/java/com/example/demo/controller/UserController.java
+++ demo/demo/src/main/java/com/example/demo/controller/UserController.java
@@ -23,7 +23,7 @@ public class UserController {
     }
 
     @GetMapping("/getUserById/{id}")
-    public UserD getUserById(@PathVariable int id) { // Mapping path variable to id
+    public UserDTO getUserById(@PathVariable int id) { // Mapping path variable to id
 
         return userService.getUserById(id);
     }
